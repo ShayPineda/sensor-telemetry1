67c125edd6f38d62a6ee41a8c70bfc528b86d83e
Sheney Pineda <cornejopineda@chapman.edu>
2026-06-03 18:41:28 -0700

    add humidity sensor and update CSV headers

--- sensor_logger.py
+++ sensor_logger.py
@@ -1,25 +1,27 @@
 import random
 import datetime
-import time
+import time .
+
 from datetime import datetime
 import os
 
 with open("sensor_log.csv", "a") as f:
     if not os.path.exists("sensor_log.csv") or os.path.getsize("sensor_log.csv") == 0:
-        f.write("timestamp,temperature\n")
+        f.write("timestamp,temperature,humidity\n")
 
     for i in range(1, 11):
         rand_temp = round(random.uniform(20.00, 35.00), 1)
+        rand_humi = round(random.uniform(30.00, 90.00), 1)
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
 
-        print (f"current time: {current_time} Temp: {rand_temp}")
+        print (f"current time: {current_time} Temp: {rand_temp} Humidity: {rand_humi}")
 
 
         if i < 10:
             time.sleep(1)
         
-        f.write(f"{current_time}, {rand_temp}\n")
+        f.write(f"{current_time}, {rand_temp}, {rand_humi}\n")
 
 
 
