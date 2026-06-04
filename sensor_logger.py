73977056e6d958895e2b978739c4b75103637731
Sheney Pineda <cornejopineda@chapman.edu>
2026-06-03 18:10:57 -0700

    initial commit: sensor data logger

---
+++ sensor_logger.py
@@ -0,0 +1,27 @@
+import random
+import datetime
+import time
+from datetime import datetime
+import os
+
+with open("sensor_log.csv", "a") as f:
+    if not os.path.exists("sensor_log.csv") or os.path.getsize("sensor_log.csv") == 0:
+        f.write("timestamp,temperature\n")
+
+    for i in range(1, 11):
+        rand_temp = round(random.uniform(20.00, 35.00), 1)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+
+        print (f"current time: {current_time} Temp: {rand_temp}")
+
+
+        if i < 10:
+            time.sleep(1)
+        
+        f.write(f"{current_time}, {rand_temp}\n")
+
+
+
+
+
