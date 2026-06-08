60c779a55207267ac3a5f37a396f7dcd4a24686e
Sheney Pineda <cornejopineda@chapman.edu>
2026-06-08 15:19:48 -0700

    add matplotlib visualization, generate sensor_chart.png

--- sensor_logger.py
+++ sensor_logger.py
@@ -3,6 +3,7 @@ import datetime
 import time 
 from datetime import datetime
 import os
+import matplotlib.pyplot as plt
 
 def generate_readings():
     rand_temp = round(random.uniform(20.00, 35.00), 1)
@@ -19,10 +20,15 @@ def log_readings():
         if not os.path.exists("sensor_log.csv") or os.path.getsize("sensor_log.csv") == 0:
             f.write("timestamp,temperature,humidity\n")
 
+        temperatures = []
+        humidities = []
+
         for i in range(1, 11):
             rand_temp, rand_humi = generate_readings()
             current_time = get_time()
             print (f"current time: {current_time} Temp: {rand_temp} Humidity: {rand_humi}")
+            temperatures.append(rand_temp)
+            humidities.append(rand_humi)
 
 
 
@@ -30,10 +36,22 @@ def log_readings():
                 time.sleep(1)
             
             f.write(f"{current_time}, {rand_temp}, {rand_humi}\n")
+        plot_readings(temperatures, humidities)
+
+
+def plot_readings(temperatures, humidities):
+    readings = list(range(1,11))
+
+    plt.plot(readings, temperatures, label = "Temperature (°C)")
+    plt.plot(readings, humidities, label = "Humidity")
+    plt.title ("Temp and Humitity")
+    plt.xlabel("Reading")
+    plt.ylabel("Temperature (°C) / Humidity (%)")
+    plt.legend()
+    plt.savefig("sensor_chart.png")
+
 
 log_readings()
-
-
 
 
 
